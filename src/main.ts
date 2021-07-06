e0ce535077f01e0d1a13db863c7660e7787e3642
tanzhuo <tanzhuo@aliyun.com>
2021-07-06 23:22:14 +0800

    完善部分配置文件

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-import installElementPlus from './plugins/element'
+import ElementPlus from 'element-plus'
 
 const app = createApp(App)
-installElementPlus(app)
+app.use(ElementPlus)
 app.use(store).use(router).mount('#app')
