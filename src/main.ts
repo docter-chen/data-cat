d4f134d8a3e9046e44c4846c1ce7f9e49689a4b3
谭卓 <tanzhuo@aliyun.com>
2021-07-07 12:01:14 +0800

    完善部分依赖

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,31 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import ElementPlus from 'element-plus'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import styleImport from 'vite-plugin-style-import'
 
 const app = createApp(App)
 app.use(ElementPlus)
 app.use(store).use(router).mount('#app')
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    styleImport({
+      libs: [
+        {
+          libraryName: 'element-plus',
+          esModule: true,
+          ensureStyleFile: true,
+          resolveStyle: (name) => {
+            return `element-plus/lib/theme-chalk/${name}.css`
+          },
+          resolveComponent: (name) => {
+            return `element-plus/lib/${name}`
+          }
+        }
+      ]
+    })
+  ]
+})
